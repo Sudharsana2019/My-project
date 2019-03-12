160a245d241d9578b031f7e4d9c082cb6e53dac4
Sudharsan <sri2sudharsan91@gmail.com>
2019-03-12 06:17:28 -0400

    Hi

--- src/com/myproject/tests/Demo_SpecifiedDateinClearTripAPP.java
+++ src/com/myproject/tests/Demo_SpecifiedDateinClearTripAPP.java
@@ -15,56 +15,6 @@ public class Demo_SpecifiedDateinClearTripAPP
 
 	public static void main(String[] args)
 	{
-		/*String doj = "15/Oct/2020";
-		String[] temp = doj.split("/");
-		String dt = temp[0];
-		String month = temp[1];
-		String year = temp[2];
-		String calyear,calmonth,caldt;
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ssrinivasan\\Downloads\\chromedriver.exe");	
-		WebDriver driver= new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.get("https://www.cleartrip.com/");
-		driver.manage().window().maximize();
-		driver.findElement(By.id("DepartDate")).click();
-		calyear = driver.findElement(By.className("ui-datepicker-year")).getText();
-	while (!calyear.equals(year))
-	{
-		driver.findElement(By.className("nextMonth")).click();
-        calyear = driver.findElement(By.className("ui-datepicker-year")).getText();		
-       }
-	calmonth = driver.findElement(By.className("ui-datepicker-month")).getText();
-	while (!calmonth.equalsIgnoreCase(month)) 
-	{
-		driver.findElement(By.className("nextMonth")).click();
-		calmonth = driver.findElement(By.className("ui-datepicker-month")).getText();
-	}
-	WebElement cal;
-	cal = driver.findElement(By.className("calendar"));
-	List<WebElement> rows,cols;
-	rows= driver.findElements(By.tagName("tr"));
-	boolean flag = false;
-	for (int i = 1; i < rows.size(); i++) 
-	{
-		cols= rows.get(i).findElements(By.tagName("td"));
-		for (int j = 0; j < cols.size(); j++) 
-		{
-		 caldt= cols.get(j).getText();
-		if (caldt.equals(dt)) 
-		{
-		cols.get(j).click();
-		flag = true;
-		break;
-		}
-		}
-		if (flag) 
-		{
-		break;	
-		}
-		}
-	    }
-        }/*
-/*********************************************************************/
 		String doj="15/August/2020";
  		String[] temp=doj.split("/");
 		String dt=temp[0];
